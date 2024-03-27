fed8b3e35419191d949414e120dce8a2cbcb671f
farzamshahid <farzamshahid6@gmail.com>
2024-03-27 13:17:14 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import ScrollIndicator from './scroll-indicator.jsx'
+function App() {
+
+  return (
+    <>
+      <ScrollIndicator url={"https://dummyjson.com/products?limit=100"} />
+    </>
+  )
+}
+
+export default App
